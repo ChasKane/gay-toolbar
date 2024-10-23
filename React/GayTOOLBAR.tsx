fcb15eee15c5e05bcd2b0e6240b7201b758f2a51
ChasKane <LawrenceCKane@gmail.com>
2024-10-23 02:54:09 -0600

    fix cursor-behind-toolbar bug, simplify main.tsx drastically

--- React/GayTOOLBAR.tsx
+++ React/GayTOOLBAR.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { RefObject, useEffect, useRef } from 'react';
 import ButtonGrid from './Grid/ButtonGrid';
 import GaySettings from './Settings/GaySettings';
 import { useEditor, useSettings } from './StateManagement';
+import { Platform } from 'obsidian';
 
 const GayToolbar: React.FC = () => {
     const isEditing = useEditor(state => state.isEditing);
     const { backgroundColor: { r, g, b }, opacity, customBackground } = useSettings();
 
+    const ref: RefObject<HTMLDivElement> = useRef(null)
+
+    useEffect(() => {
+        if (Platform.isMobile)
+            return;
+        const statusBar: HTMLDivElement | null = document.querySelector('.status-bar')
+        if (statusBar)
+            statusBar.style.bottom = (ref.current?.getBoundingClientRect().height || 0) + 'px'
+    })
+
     return (
-        <div style={{ background: customBackground || `rgba(${[r, g, b, opacity].join(',')}` }} className='gay-toolbar'>
+        <div ref={ref} style={{ background: customBackground || `rgba(${[r, g, b, opacity].join(',')}` }} className='gay-toolbar'>
             {isEditing && <GaySettings />}
             <ButtonGrid />
         </div>
--- main.tsx
+++ main.tsx
@@ -11,7 +11,6 @@ export default class GayToolbarPlugin extends Plugin {
     toolbarNode: HTMLElement;
     observer: MutationObserver;
     unsubscribePositionStore: () => void;
-    currentLeaf: WorkspaceLeaf;
 
     async onload() {
         this.loadSettings()
@@ -32,64 +31,18 @@ export default class GayToolbarPlugin extends Plugin {
             },
         });
 
-        // when there's no keyboard, navbar is absolutely positioned and toolbar must be rendered above it
-        // when keyboard is visible, navbar disappears and toolbar needs to be in document flow so cursor doesn't go underneath it
-        this.observer = new MutationObserver(mutations => mutations.forEach(mutation => {
-            if (mutation.type === 'childList') {
-                if (mutation.addedNodes.length) {
-                    mutation.addedNodes.forEach(node => {
-                        if (node.nodeType === 1
-                            && this.toolbarNode
-                            && (node as Element).classList.contains('mobile-navbar')
-                        ) {
-                            // this.toolbarNode.style.bottom = ((node as Element).clientHeight || 0) + 'px';
-                        }
-                    })
-                }
-                if (mutation.removedNodes.length) {
-                    mutation.removedNodes.forEach(node => {
-                        if (node.nodeType === 1
-                            && this.toolbarNode
-                            && (node as Element).classList.contains('mobile-navbar')
-                        ) {
-                            // this.toolbarNode.style.bottom = '0px';
-                        }
-                    })
-                }
+        this.app.workspace.onLayoutReady(() => {
+            this.toolbarRoot?.unmount?.();
+            this.toolbarNode?.remove();
+
+            const parentNode = document.querySelector('.app-container')
+            if (parentNode) {
+                this.toolbarNode = createDiv('gay-toolbar-container');
+                this.toolbarRoot = createRoot(this.toolbarNode);
+                this.toolbarRoot.render(<GayToolbar />);
+                parentNode.insertBefore(this.toolbarNode, parentNode.querySelector('.status-bar'));
             }
-        }))
-
-        this.app.workspace.on('active-leaf-change', this.initUI.bind(this))
-
-        this.app.workspace.onLayoutReady(this.initUI.bind(this));
-    }
-
-    initUI(e) {
-        console.log('initUI', e === this.currentLeaf, e, this.currentLeaf)
-        if (e === this.currentLeaf)
-            return;
-        this.currentLeaf = e;
-        this.toolbarRoot?.unmount?.();
-        this.toolbarNode?.remove();
-        this.observer?.disconnect();
-
-        const appContainer = document.querySelector('.app-container')
-        if (appContainer)
-            this.observer.observe(appContainer, { childList: true })
-
-        const parentNode = document.querySelector('.mod-active .workspace-tab-container');
-        if (parentNode) {
-            this.toolbarNode = createDiv('gay-toolbar-container');
-            this.toolbarNode.style.position = 'absolute';
-            const navbar: HTMLElement | null = document.querySelector('.mobile-navbar');
-            // on desktop, when emulating mobile, navbar's position is static
-            if (navbar?.getCssPropertyValue('position') === 'absolute') {
-                // this.toolbarNode.style.bottom = (navbar.clientHeight) + 'px';
-            }
-            this.toolbarRoot = createRoot(this.toolbarNode);
-            this.toolbarRoot.render(<GayToolbar />);
-            parentNode.appendChild(this.toolbarNode);
-        }
+        });
     }
 
     async saveSettings(newSettings: GayToolbarSettings) {
